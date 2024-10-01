a03073ad8ffe6c82cd05f8989e60cd4b6348d0a3
joaocarloslima <joaocarloslima@me.com>
2024-10-01 11:03:48 -0300

    cadastrar tarefa

--- src/main/java/br/com/fiap/epictaskn/task/TaskController.java
+++ src/main/java/br/com/fiap/epictaskn/task/TaskController.java
@@ -1,8 +1,12 @@
 package br.com.fiap.epictaskn.task;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -20,6 +24,20 @@ public class TaskController {
         var tasks = taskService.findAll();
         model.addAttribute("tasks", tasks);
         return "index";
+    }
+
+    @GetMapping("/form")
+    public String form(Task task){
+        return "form";
+    }
+
+    @PostMapping("/task")
+    public String create(@Valid Task task, BindingResult result, RedirectAttributes redirectAttributes){
+        if (result.hasErrors()) return "form";
+
+        taskService.create(task);
+        redirectAttributes.addFlashAttribute("message", "Tarefa cadastrada com sucesso");
+        return "redirect:/";
     }
 
 }

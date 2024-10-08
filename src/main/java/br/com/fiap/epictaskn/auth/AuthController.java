c03bec030622f54dc6a885a7e82f52c38ff4063e
joaocarloslima <joaocarloslima@me.com>
2024-10-08 12:01:10 -0300

    OAUTH2

---
+++ src/main/java/br/com/fiap/epictaskn/auth/AuthController.java
@@ -0,0 +1,18 @@
+package br.com.fiap.epictaskn.auth;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(){
+        return "logout";
+    }
+}
--- src/main/java/br/com/fiap/epictaskn/task/TaskController.java
+++ src/main/java/br/com/fiap/epictaskn/task/TaskController.java
@@ -2,6 +2,8 @@ package br.com.fiap.epictaskn.task;
 
 import jakarta.validation.Valid;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -26,9 +28,10 @@ public class TaskController {
     }
 
     @GetMapping
-    public String index(Model model){
+    public String index(Model model, @AuthenticationPrincipal OAuth2User user){
         var tasks = taskService.findAll();
         model.addAttribute("tasks", tasks);
+        model.addAttribute("user", user);
         return "index";
     }
 
